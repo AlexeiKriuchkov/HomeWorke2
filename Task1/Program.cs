87f6061aae2b1cd2c6b2cb9be99e73189593fefa
Алексей <kruchkov_alex@mail.ru>
2023-02-07 12:01:50 +0300

    Task1.2

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿ // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine();
+
+Console.Write("Введите число: ");
+
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+    int Digit=0;
+    Digit = number;
+    Digit = Digit/10;
+    Digit = Digit%10;
+    Console.WriteLine("Второй цифрой числа " + number + " является " + Digit);
+
+}
+
+Console.WriteLine();
